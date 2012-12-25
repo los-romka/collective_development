1eefbc52c66a41b014cbf9a68831e8089617aef5
Daniil <forzalino@gmail.com>
2012-12-25 22:25:39 +1100

    #50
    Составлен мануал ( залит в appendix ) и создан новый проект ( залит в
    code )

--- Code/coldev_vsch/vsch/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.http import Http404
-from django.shortcuts import render_to_response
-from vsch.models import *
-
-def home(request):
-	return render_to_response('index.html')
-
-
