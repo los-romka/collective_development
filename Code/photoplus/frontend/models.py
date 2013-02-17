5d97ee0b353eccdd3ad63dc3b0f834d4729325df
Interbellum <forzalino@gmail.com>
2013-02-18 00:17:04 +1100

    sceleton refactoring

--- Code/photoplus/frontend/models.py
+++ Code/photoplus/frontend/models.py
@@ -1,30 +1,53 @@
+
+
+
 from django.db import models
 
-class Post(models.Model):
-	image_url = models.CharField(max_length=200)
-	renew = models.DateTimeField('date of renewal')
-
-	def __unicode__(self):
-		return self.name
 
 
-class Tag(models.Model):
-	name = models.CharField(max_length=200)
-	posts = models.ManyToManyField(Post)
-
-	def __unicode__(self):
-		return self.name
 
 
-class Album(models.Model):
-	name = models.CharField(max_length=200)
-	tags = models.ManyToManyField(Tag)
+# Contains information about single activity unit ( photo )
 
-	def __unicode__(self):
-		return self.name
+class Post ( models.Model ):
+
+	image_url = models.CharField( max_length=200 )
+	renew = models.DateTimeField( 'date of renewal' )
 
 
-class Admin(models.Model):
-	login = models.CharField(max_length=200)
-	hashpass = models.CharField(max_length=200)
-	renew = models.DateTimeField('date of total renewal')
+
+
+
+# Contains information about a tag
+
+class Tag ( models.Model ):
+
+	name = models.CharField( max_length=200 )
+	posts = models.ManyToManyField( Post )
+
+
+
+
+
+
+# Contains information about an album
+
+class Album ( models.Model ):
+
+	name = models.CharField( max_length=200 )
+	tags = models.ManyToManyField( Tag )
+
+#	def __unicode__( self ):
+#		return self.name
+
+
+
+
+
+# Contains admin security data
+
+class Admin ( models.Model ):
+
+	login = models.CharField( max_length=200 )
+	hashpass = models.CharField( max_length=200 )
+	renew = models.DateTimeField( 'date of total renewal' )
