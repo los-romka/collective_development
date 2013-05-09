7aaa5f09d41628a3d14e7a4fa409232c7b246fe3
PopovAlexey <lexalexa-setset@mail.ru>
2013-05-09 14:03:18 +1100

    add captcha2

--- Code/photoplus/frontend/admin.py
+++ Code/photoplus/frontend/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from frontend.models import *
 
+
+
 class AlbumAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Album, AlbumAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Message, MessageAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+	pass	
+admin.site.register(Author, AuthorAdmin)
+
+class PriceAdmin(admin.ModelAdmin):
+	pass	
+fields = ('price', ('size', 'on'))
+admin.site.register(Price, PriceAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+	pass	
+fields = ('status')
+admin.site.register(Order, OrderAdmin)
+
--- Code/photoplus/frontend/models.py
+++ Code/photoplus/frontend/models.py
@@ -11,8 +11,10 @@ from django.db import models
 
 class Post ( models.Model ):
 
-	image_url = models.CharField( max_length=200 )
-	renew = models.DateTimeField( 'date of renewal' )
+	post_title = models.CharField( max_length=35 )
+    	renew = models.DateTimeField( 'date of renewal' )
+   	image_url = models.CharField( max_length=200 )
+    	post_url = models.CharField( max_length=200 )
 
 
 
@@ -50,3 +52,36 @@ class Admin ( models.Model ):
 	login = models.CharField( max_length=200 )
 	hashpass = models.CharField( max_length=200 )
 	renew = models.DateTimeField( 'date of total renewal' )
+
+class Author( models.Model ):
+
+	photo_url = models.CharField( max_length=200 )
+	information = models.TextField(max_length=10000)
+	email = models.EmailField()
+
+class Message( models.Model ):
+
+	email = models.EmailField( max_length=200 )
+	subject = models.CharField( max_length=200)
+	information = models.TextField(max_length=10000)
+ 
+class Price ( models.Model ):
+	price = models.PositiveSmallIntegerField(max_length=4, default=0)
+	size = models.PositiveSmallIntegerField( max_length=10, default=10 )
+ 	on = models.PositiveSmallIntegerField( max_length=10, default=10 )
+
+class Order ( models.Model ):
+	Order_choices = (
+	    ('RECEIVED ', 'RECEIVED '),
+	    ('SHIPPED  ', 'SHIPPED  '),
+	    ('ORDER PROCESSING', 'ORDER PROCESSING'),
+	    ('CANCELLED', 'CANCELLED'),
+	)
+	name = models.CharField( max_length=150)
+	adress = models.CharField( max_length=70)
+ 	photo_id = models.CharField( max_length=200)
+	price = models.PositiveSmallIntegerField(max_length=4, default=0)
+	size = models.CharField( max_length=22)
+	status = models.CharField(max_length=20,choices=Order_choices, default='RECEIVED')
+
+	 		 	
